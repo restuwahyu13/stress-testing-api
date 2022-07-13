c6e38392e3828c2d364e8976763ca2f8206a63ff
Restu Wahyu Saputra <restuwahyu13@gmail.com>
2022-07-14 01:15:08 +0700

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+accessToken=""
+concureny=10
+timeout="5M"
+requestTime=30
+userAgent="PostmanRuntime/7.29.0"
+contentType="json"
+randomTextUrl="url.txt"
+
+# siege -v -i -c ${concureny} -r ${requestTime} -t ${timeout} -A ${userAgent} -T ${contentType} -H "Authorization:Bearer $accessToken" -f "$randomTextUrl"
+# siege -v -i -c ${concureny} -r ${requestTime} -t ${timeout} -A ${userAgent} -T ${contentType} -f "$randomTextUrl"
+exit

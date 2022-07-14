3d76a6ebaf64555e667b587280205a3add796288
Restu Wahyu Saputra <restuwahyu13@gmail.com>
2022-07-14 11:13:19 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 accessToken=""
-concureny=10
-timeout="5M"
-requestTime=30
 userAgent="PostmanRuntime/7.29.0"
+totalUser=10
+requestTimeout=30
+sessionEnd="5M"
 contentType="json"
 randomTextUrl="urls.txt"
 
-# siege -v -i -c ${concureny} -r ${requestTime} -t ${timeout} -A ${userAgent} -T ${contentType} -H "Authorization:Bearer $accessToken" -f "$randomTextUrl"
-# siege -v -i -c ${concureny} -r ${requestTime} -t ${timeout} -A ${userAgent} -T ${contentType} -f "$randomTextUrl"
+# siege -v -i -c ${totalUser} -r ${requestTimeout} -t ${sessionEnd} -A ${userAgent} -T ${contentType} -H "Authorization:Bearer $accessToken" -f "$randomTextUrl"
+siege -v -i -c ${totalUser} -r ${requestTimeout} -t ${sessionEnd} -A ${userAgent} -T ${contentType} -f "$randomTextUrl"
 exit
